bfd4a8816390a63973ecca3a7ce2c6b1ffee1b9d
bibagray <142665115+bibagray@users.noreply.github.com>
2023-08-19 14:55:48 +0100

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+print('19/09/2023')
+print('Dear Beatrice,')
+print('You are a superstar!')
+print('I bet you didn not expect you could code! Congratulations!')
+print('You will be able to get a job that allows you to work from anywhere in the world, so you can explore it!')
+print('Love from, Beatrice 💖')
