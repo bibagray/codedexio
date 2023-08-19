15d1c97eb42cfde6c752da5ce4e955ae2661dc1f
bibagray <142665115+bibagray@users.noreply.github.com>
2023-08-19 14:52:13 +0100

    Update and rename pattern.py to initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+print('hi!') # I'm a comment
+print('my name is beatrice') #i grew up in Germany
+print('BBBBB      GGGGGGGGG')
+print('B   BB     G')
+print('BBBBB      G  GGGGGG')
+print('B   BB     G   G  G')
+print('B   BB     G   G  G')
+print('BBBBB      GGGGG')
