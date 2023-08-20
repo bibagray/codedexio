d136525eea86dccdbbeed9267981136e860ac884
bibagray <142665115+bibagray@users.noreply.github.com>
2023-08-20 11:49:19 +0100

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,10 @@
+# conversion code
+# codeexcurrency 💖
+remainingpesos = int(input('What do you have left in pesos?'))
+reaminingsoles = int(input('What do you have left in soles?'))
+reaminingreais = int(input('What do you have left in reais?'))
+pesos = remainingpesos * 0.00024
+soles = reaminingsoles * 0.27
+reais = reaminingreais * 0.20
+total = (pesos + soles + reais)
+print(total)
