e49eb6dea4a509f82b00b6061621b3063adcf1e8
bibagray <142665115+bibagray@users.noreply.github.com>
2023-08-20 11:32:44 +0100

    Create pythagorean.py

---
+++ pythagorean.py
@@ -0,0 +1,6 @@
+# this will show pythagorus 
+a = int(input('what is the length of the short side?'))
+b = int(input('what is the length of the other short side?'))
+number = (a**2) + (b**2) 
+c = number**(1/2)
+print(c)
