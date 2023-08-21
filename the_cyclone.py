8f4c31a233cf340ed396f0ea1f59ad8fc5b80e81
bibagray <142665115+bibagray@users.noreply.github.com>
2023-08-21 21:09:52 +0100

    Update the_cyclone.py

--- the_cyclone.py
+++ the_cyclone.py
@@ -1,12 +1,11 @@
-# this will code for the cyclone rollercoaster 
 # cyclone height restriction 💖
 height = int(input('How tall are you ?'))
 credits = int(input('How many credits do you have?'))
-if height <= 137 and credits >= 10
+if height >= 137 and credits >= 10:
   print('Enjoy the ride!')
-elif height < 137 and credits <= 10
+elif height > 137 and credits <= 10:
   print('You are not tall enough to ride')
-elif height <= 137 and credits > 10
+elif height >= 137 and credits < 10:
   print('You do not have enough credits to ride')
-else height > 137 and credits > 10
+else:
   print('You are not tall enough and do not have enough credits')
